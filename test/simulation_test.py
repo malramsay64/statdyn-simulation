1caee474ccbe43f66842e3b6223a4814e360a264
Malcolm Ramsay <malramsay64@gmail.com>
2018-04-03 14:24:25 +1000

    Use pytest fixtures in simulation tests
    Update the suite of tests for the simulations.
    
    This also removes the xfail from the test check for the placement of
    files during a simulation. Issue was with the simulation parameters not
    including dynamics=True.

--- test/simulation_test.py
+++ test/simulation_test.py
@@ -7,182 +7,116 @@
 # Distributed under terms of the MIT license.
 """Test the simulation module."""
 
-import gc
-import os
 import subprocess
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import hoomd
 import pytest
-from hypothesis import given, settings
-from hypothesis.strategies import integers, tuples
-
-from sdrun import crystals, molecules
-from sdrun import equilibrate, initialise, simrun
+from sdrun.crystals import CRYSTAL_FUNCS
+from sdrun.equilibrate import equil_crystal
+from sdrun.initialise import init_from_crystal, init_from_none
+from sdrun.molecules import MOLECULE_DICT
 from sdrun.params import SimulationParams, paramsContext
+from sdrun.simrun import run_npt
 
-OUTDIR = Path('test/tmp')
-OUTDIR.mkdir(exist_ok=True)
 HOOMD_ARGS = "--mode=cpu"
-PARAMETERS = SimulationParams(
-    temperature=0.4,
-    num_steps=100,
-    crystal=crystals.TrimerPg(),
-    outfile_path=OUTDIR,
-    outfile=OUTDIR / 'testout',
-    dynamics=False,
-    hoomd_args=HOOMD_ARGS,
-    minimize=True,
-)
+
+
+@pytest.fixture(params=MOLECULE_DICT.values(), ids=MOLECULE_DICT.keys())
+def sim_params(request):
+    with TemporaryDirectory() as tmp_dir:
+        yield SimulationParams(
+            temperature=0.4,
+            num_steps=100,
+            molecule=request.param(),
+            output=tmp_dir,
+            outfile=Path(tmp_dir) / 'testout',
+            dynamics=False,
+            hoomd_args=HOOMD_ARGS,
+            minimize=True,
+        )
+
+
+@pytest.fixture(params=CRYSTAL_FUNCS.values(), ids=CRYSTAL_FUNCS.keys())
+def sim_params_crystal(request):
+    with TemporaryDirectory() as tmp_dir:
+        yield SimulationParams(
+            temperature=0.4,
+            num_steps=100,
+            crystal=request.param(),
+            output=tmp_dir,
+            outfile=Path(tmp_dir) / 'testout',
+            dynamics=False,
+            hoomd_args=HOOMD_ARGS,
+            minimize=True,
+        )
 
 
 @pytest.mark.simulation
-@pytest.mark.parametrize('molecule', molecules.MOLECULE_LIST)
-def test_run_npt(molecule):
+def test_run_npt(sim_params):
     """Test an npt run."""
-    with paramsContext(PARAMETERS, molecule=molecule):
-        snapshot = initialise.init_from_none(PARAMETERS)
-        simrun.run_npt(
-            snapshot=snapshot,
-            context=hoomd.context.initialize(''),
-            sim_params=PARAMETERS,
-        )
-        assert True
+    run_npt(
+        snapshot=init_from_none(sim_params),
+        context=hoomd.context.initialize(HOOMD_ARGS),
+        sim_params=sim_params,
+    )
 
 
-@given(integers(max_value=10, min_value=1))
-@settings(max_examples=5, deadline=None)
-@pytest.mark.hypothesis
-def test_run_multiple_concurrent(max_initial):
-    """Test running multiple concurrent."""
-    with paramsContext(PARAMETERS, max_initial=max_initial):
-        snapshot = initialise.init_from_file(
-            Path('test/data/Trimer-13.50-3.00.gsd'),
-            PARAMETERS.molecule,
-            hoomd_args=PARAMETERS.hoomd_args,
-        )
-        simrun.run_npt(
-            snapshot, context=hoomd.context.initialize(''), sim_params=PARAMETERS
-        )
-    assert True
-    gc.collect()
-
-
-@pytest.mark.parametrize('molecule', molecules.MOLECULE_LIST)
-def test_thermo(molecule):
-    """Test the _set_thermo function works.
-
-    There are many thermodynamic values set in the function and ensuring that
-    they can all be initialised is crucial to a successful simulation.
-    """
-    output = Path('test/tmp')
-    output.mkdir(exist_ok=True)
-    with paramsContext(PARAMETERS, molecule=molecule):
-        snapshot = initialise.init_from_none(PARAMETERS)
-        simrun.run_npt(
-            snapshot, context=hoomd.context.initialize(''), sim_params=PARAMETERS
-        )
-        assert True
-
-
-@given(tuples(integers(max_value=30, min_value=5), integers(max_value=5, min_value=1)))
-@settings(max_examples=10, deadline=None)
-def test_orthorhombic_sims(cell_dimensions):
+@pytest.mark.simulation
+@pytest.mark.parametrize('cell_dimensions', range(1, 5))
+def test_orthorhombic_sims(cell_dimensions, sim_params_crystal):
     """Test the initialisation from a crystal unit cell.
 
     This also ensures there is no unusual things going on with the calculation
     of the orthorhombic unit cell.
     """
-    cell_dimensions = cell_dimensions[0], cell_dimensions[1] * 6
-    output = Path('test/tmp')
-    output.mkdir(exist_ok=True)
-    with paramsContext(PARAMETERS, cell_dimensions=cell_dimensions):
-        snap = initialise.init_from_crystal(PARAMETERS)
-    snap = equilibrate.equil_crystal(snap, sim_params=PARAMETERS)
-    simrun.run_npt(snap, context=hoomd.context.initialize(''), sim_params=PARAMETERS)
-    assert True
-    gc.collect()
+    sim_params = sim_params_crystal
+    cell_dimensions = cell_dimensions * 6, cell_dimensions * 6
+    with paramsContext(sim_params, cell_dimensions=cell_dimensions):
+        snap = init_from_crystal(sim_params)
+    snap = equil_crystal(snap, sim_params=sim_params)
+    run_npt(snap, context=hoomd.context.initialize(HOOMD_ARGS), sim_params=sim_params)
 
 
-@pytest.mark.parametrize('molecule', molecules.MOLECULE_LIST)
-@pytest.mark.xfail
-def test_equil_file_placement(molecule):
-    outdir = Path('test/output')
-    outfile = outdir / 'test_equil'
-    current = list(Path.cwd().glob('*'))
-    for i in outdir.glob('*'):
-        os.remove(str(i))
-    with paramsContext(
-        PARAMETERS,
-        outfile_path=outdir,
-        outfile=outfile,
-        temperature=4.00,
-        molecule=molecule,
-    ):
-        snapshot = initialise.init_from_none(PARAMETERS)
-        equilibrate.equil_liquid(snapshot, PARAMETERS)
-        assert current == list(Path.cwd().glob('*'))
-        assert Path(outfile).is_file()
-    for i in outdir.glob('*'):
-        os.remove(str(i))
-
-
-@pytest.mark.parametrize('molecule', molecules.MOLECULE_LIST)
-@pytest.mark.xfail
-def test_file_placement(molecule):
+@pytest.mark.simulation
+def test_file_placement(sim_params):
     """Ensure files are located in the correct directory when created."""
-    outdir = Path('test/output')
-    current = list(Path.cwd().glob('*'))
-    for i in outdir.glob('*'):
-        os.remove(str(i))
-    with paramsContext(
-        PARAMETERS,
-        outfile_path=outdir,
-        dynamics=True,
-        temperature=3.00,
-        molecule=molecule,
-    ):
-        snapshot = initialise.init_from_none(PARAMETERS)
-        simrun.run_npt(snapshot, hoomd.context.initialize(''), sim_params=PARAMETERS)
-        assert current == list(Path.cwd().glob('*'))
-        sim_params = {
-            'molecule': PARAMETERS.molecule,
-            'pressure': PARAMETERS.pressure,
-            'temperature': PARAMETERS.temperature,
-        }
-        assert (
-            outdir /
-            '{molecule}-P{pressure:.2f}-T{temperature:.2f}.gsd'.format(**sim_params)
-        ).is_file(
+    with paramsContext(sim_params, dynamics=True):
+        run_npt(
+            init_from_none(sim_params),
+            hoomd.context.initialize(HOOMD_ARGS),
+            sim_params=sim_params,
         )
-        assert (
-            outdir /
-            'dump-{molecule}-P{pressure:.2f}-T{temperature:.2f}.gsd'.format(
-                **sim_params
-            )
-        ).is_file(
-        )
-        assert (
-            outdir /
-            'thermo-{molecule}-P{pressure:.2f}-T{temperature:.2f}.log'.format(
-                **sim_params
-            )
-        ).is_file(
-        )
-        assert (
-            outdir /
-            'trajectory-{molecule}-P{pressure:.2f}-T{temperature:.2f}.gsd'.format(
-                **sim_params
-            )
-        ).is_file(
-        )
-    for i in outdir.glob('*'):
-        os.remove(str(i))
+    params = {
+        'molecule': sim_params.molecule,
+        'pressure': sim_params.pressure,
+        'temperature': sim_params.temperature,
+    }
+    outdir = Path(sim_params.output)
+    print(list(outdir.glob('*')))
+    assert (
+        outdir / '{molecule}-P{pressure:.2f}-T{temperature:.2f}.gsd'.format(**params)
+    ).is_file()
+    assert (
+        outdir /
+        'dump-{molecule}-P{pressure:.2f}-T{temperature:.2f}.gsd'.format(**params)
+    ).is_file()
+    assert (
+        outdir /
+        'thermo-{molecule}-P{pressure:.2f}-T{temperature:.2f}.log'.format(**params)
+    ).is_file()
+    assert (
+        outdir /
+        'trajectory-{molecule}-P{pressure:.2f}-T{temperature:.2f}.gsd'.format(**params)
+    ).is_file()
 
 
+@pytest.mark.simulation
 @pytest.mark.parametrize('pressure, temperature', [(1.0, 1.8), (13.5, 3.00)])
-def test_interface(pressure, temperature):
+def test_interface(sim_params, pressure, temperature):
     init_temp = 0.4
+    outdir = str(sim_params.output)
     create_command = [
         'sdrun',
         'create',
@@ -198,12 +132,13 @@ def test_interface(pressure, temperature):
         '--steps',
         '1000',
         '--output',
-        OUTDIR,
+        outdir,
         '-vvv',
         '--hoomd-args',
         '"--mode=cpu"',
         str(
-            OUTDIR / 'create_interface-P{:.2f}-T{:.2f}.gsd'.format(pressure, init_temp)
+            Path(outdir) /
+            'create_interface-P{:.2f}-T{:.2f}.gsd'.format(pressure, init_temp)
         ),
     ]
     melt_command = [
@@ -218,17 +153,19 @@ def test_interface(pressure, temperature):
         '--temperature',
         '{}'.format(temperature),
         '--output',
-        OUTDIR,
+        outdir,
         '--steps',
         '1000',
         '-vvv',
         '--hoomd-args',
         '"--mode=cpu"',
         str(
-            OUTDIR / 'create_interface-P{:.2f}-T{:.2f}.gsd'.format(pressure, init_temp)
+            Path(outdir) /
+            'create_interface-P{:.2f}-T{:.2f}.gsd'.format(pressure, init_temp)
         ),
         str(
-            OUTDIR / 'melt_interface-P{:.2f}-T{:.2f}.gsd'.format(pressure, temperature)
+            Path(outdir) /
+            'melt_interface-P{:.2f}-T{:.2f}.gsd'.format(pressure, temperature)
         ),
     ]
     create = subprocess.run(create_command)
