ed8ca4fefc85c44ff1013601095f51e2f9eecdfa
Malcolm Ramsay <malramsay64@gmail.com>
2018-04-03 13:46:50 +1000

    Use TemporaryDirectory for all output in equilibrate_test

--- test/equilibrate_test.py
+++ test/equilibrate_test.py
@@ -10,37 +10,36 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from sdrun import equilibrate, initialise, params
-from sdrun.crystals import TrimerPg
+import pytest
+from sdrun.crystals import CRYSTAL_FUNCS
+from sdrun.equilibrate import equil_crystal, equil_interface, equil_liquid
+from sdrun.initialise import init_from_crystal
+from sdrun.params import SimulationParams
 
 
-@pytest.fixture
-def sim_params():
+@pytest.fixture(params=CRYSTAL_FUNCS.values(), ids=CRYSTAL_FUNCS.keys())
+def sim_params(request):
     with TemporaryDirectory() as tmp_dir:
-        return params.SimulationParams(
+        yield SimulationParams(
             temperature=0.4,
             num_steps=100,
-            crystal=TrimerPg(),
-            outfile_path=tmp_dir,
-            cell_dimensions=(10, 10),
+            crystal=request.param(),
+            output=Path(tmp_dir),
+            cell_dimensions=(10, 12),
             outfile=Path(tmp_dir) / 'out.gsd',
         )
 
 
-def init_frame(sim_params):
-    return initialise.init_from_crystal(sim_params)
-
-
 def test_equil_crystal(sim_params):
-    equilibrate.equil_crystal(init_frame(), sim_params)
+    equil_crystal(init_from_crystal(sim_params), sim_params)
     assert True
 
 
 def test_equil_interface(sim_params):
-    equilibrate.equil_interface(init_frame(), sim_params)
+    equil_interface(init_from_crystal(sim_params), sim_params)
     assert True
 
 
 def test_equil_liquid(sim_params):
-    equilibrate.equil_liquid(init_frame(), sim_params)
+    equil_liquid(init_from_crystal(sim_params), sim_params)
     assert True
