941504971f917c31742220ee94b269135730f429
Malcolm Ramsay <malramsay64@gmail.com>
2018-04-13 17:16:11 +1000

    Add testing for harmonic module
    This adds a simple test suite for the harmonic module to ensure that
    both the simulations actually run. The main part of the testing is
    ensuring that the box doesn't change shape during the minimisation or
    the simulation phase of the simulations.

---
+++ test/harmonic_test.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""Test harmonic module."""
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import hoomd
+import pytest
+from sdrun.crystals import CRYSTAL_FUNCS
+from sdrun.harmonic import minimize_crystal, nvt_minimize, run_harmonic
+from sdrun.params import SimulationParams
+
+
+@pytest.fixture(params=CRYSTAL_FUNCS.values(), ids=CRYSTAL_FUNCS.keys())
+def sim_params(request):
+    with TemporaryDirectory() as tmp_dir:
+        output_dir = Path(tmp_dir) / "output"
+        output_dir.mkdir(exist_ok=True)
+        yield SimulationParams(
+            temperature=0.4,
+            num_steps=100,
+            output=output_dir,
+            outfile=output_dir / "test.gsd",
+            crystal=request.param(),
+            cell_dimensions=[5],
+        )
+
+
+def test_minimize_crystal(sim_params):
+    minimize_crystal(sim_params)
+
+
+def test_nvt_minimize_box(sim_params):
+    """Ensure the box doesn't change size"""
+    snap_init = minimize_crystal(sim_params)
+    snap_final = nvt_minimize(snap_init, sim_params)
+    assert snap_init.box.Lx == snap_final.box.Lx
+    assert snap_init.box.Ly == snap_final.box.Ly
+    assert snap_init.box.Lz == snap_final.box.Lz
+    assert snap_init.box.xy == snap_final.box.xy
+    assert snap_init.box.xz == snap_final.box.xz
+    assert snap_init.box.yz == snap_final.box.yz
+
+
+def test_run_harmonic(sim_params):
+    snap_init = minimize_crystal(sim_params)
+    snap_min = nvt_minimize(snap_init, sim_params)
+    context = hoomd.context.initialize(sim_params.hoomd_args)
+    run_harmonic(snap_min, context, sim_params)
