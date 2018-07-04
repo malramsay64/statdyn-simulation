9929279adbd3f0661026cbdb85c24fceff5e9503
Malcolm Ramsay <malramsay64@gmail.com>
2018-07-04 14:59:36 +1000

    ENH Take momenta from moving particles
    When taking the momenta of particles from an interface, remove the
    particles with no momenta from the distribution of momenta. This means
    momenta are selected multiple times, however the distribution will
    remain the same.
    
    This includes additions to the test cases ensuring this function works
    as intended.

--- test/util_test.py
+++ test/util_test.py
@@ -9,11 +9,18 @@
 """Test the util module."""
 
 import hoomd
+import numpy as np
 import pytest
 from hoomd.data import boxdim, make_snapshot
 
 # TODO set_integrator, set_debug, set_dump, dump_frame, set_thermo, set_harmonic_force
-from sdrun.util import NumBodies, _get_num_bodies, get_num_mols, get_num_particles
+from sdrun.util import (
+    NumBodies,
+    _get_num_bodies,
+    get_num_mols,
+    get_num_particles,
+    randomise_momenta,
+)
 
 
 @pytest.fixture
@@ -42,3 +49,41 @@ def test_get_num_particles(create_snapshot):
 def test_get_num_mols(create_snapshot):
     num_bodies = get_num_mols(create_snapshot["snapshot"])
     assert num_bodies == create_snapshot["num_bodies"].molecules
+
+
+@pytest.fixture(params=["Interface", "No-Interface"])
+def snapshot(request):
+    snap_file = "test/data/Trimer-13.50-3.00.gsd"
+    if request.param == "Interface":
+        snapshot = hoomd.data.gsd_snapshot(snap_file)
+        num_mols = get_num_mols(snapshot)
+        stationary_particles = int(num_mols / 3)
+        snapshot.particles.velocity[:stationary_particles] = np.zeros(
+            (stationary_particles, 3), dtype=np.float32
+        )
+        snapshot.particles.angmom[:stationary_particles] = np.zeros(
+            (stationary_particles, 4), dtype=np.float32
+        )
+        return snapshot
+    return hoomd.data.gsd_snapshot("test/data/Trimer-13.50-3.00.gsd")
+
+
+@pytest.mark.parametrize("interface", [False, True])
+def test_randomise_momenta(snapshot, interface):
+    snap = randomise_momenta(snapshot, interface)
+    num_mols = get_num_mols(snapshot)
+    angmom_similarity = np.sum(
+        snap.particles.angmom[:num_mols] != snapshot.particles.angmom[:num_mols]
+    )
+    velocity_similarity = np.sum(
+        snap.particles.velocity[:num_mols] != snapshot.particles.velocity[:num_mols]
+    )
+    assert angmom_similarity < 5
+    assert velocity_similarity < 5
+    if interface:
+        assert np.any(snap.particles.velocity[:num_mols] > 0)
+        assert np.any(snap.particles.angmom[:num_mols] > 0)
+        velocity_norm = np.linalg.norm(snap.particles.velocity[:num_mols], axis=1)
+        assert np.sum(velocity_norm == 0) < 5
+        angmom_norm = np.linalg.norm(snap.particles.angmom[:num_mols], axis=1)
+        assert np.sum(angmom_norm == 0) < 5
