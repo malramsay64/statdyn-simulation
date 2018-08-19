d30744d539de0b80dcec41eb489a86eebec5ee97
Malcolm Ramsay <malramsay64@gmail.com>
2018-08-19 13:48:50 +1000

    TST Remove randomise momenta test from util

--- test/util_test.py
+++ test/util_test.py
@@ -83,49 +83,6 @@ def snapshot(request):
     return hoomd.data.gsd_snapshot("test/data/Trimer-13.50-3.00.gsd")
 
 
-@pytest.mark.parametrize("interface", [False, True])
-def test_randomise_momenta(snapshot, interface):
-    snap = randomise_momenta(snapshot, interface)
-    num_mols = get_num_mols(snapshot)
-    angmom_similarity = np.sum(
-        snap.particles.angmom[:num_mols] != snapshot.particles.angmom[:num_mols]
-    )
-    velocity_similarity = np.sum(
-        snap.particles.velocity[:num_mols] != snapshot.particles.velocity[:num_mols]
-    )
-    assert angmom_similarity < 5
-    assert velocity_similarity < 5
-    if interface:
-        assert np.any(snap.particles.velocity[:num_mols] > 0)
-        assert np.any(snap.particles.angmom[:num_mols] > 0)
-        velocity_norm = np.linalg.norm(snap.particles.velocity[:num_mols], axis=1)
-        assert np.sum(velocity_norm == 0) < 5
-        angmom_norm = np.linalg.norm(snap.particles.angmom[:num_mols], axis=1)
-        assert np.sum(angmom_norm == 0) < 5
-
-
-@pytest.mark.parametrize("seed", [0, 1, 10])
-def test_randomise_seed_same(snapshot, seed):
-    snap1 = randomise_momenta(snapshot, random_seed=seed)
-    snap2 = randomise_momenta(snapshot, random_seed=seed)
-    assert np.all(snap1.particles.velocity == snap2.particles.velocity)
-    assert np.all(snap1.particles.angmom == snap2.particles.angmom)
-
-
-def test_randomise_seed_different(snapshot):
-    num_mols = get_num_mols(snapshot)
-    snap1 = randomise_momenta(snapshot, random_seed=0)
-    snap2 = randomise_momenta(snapshot, random_seed=1)
-    angmom_similarity = np.sum(
-        snap1.particles.angmom[:num_mols] != snap2.particles.angmom[:num_mols]
-    )
-    velocity_similarity = np.sum(
-        snap1.particles.velocity[:num_mols] != snap2.particles.velocity[:num_mols]
-    )
-    assert angmom_similarity < 5
-    assert velocity_similarity < 5
-
-
 def test_z2quaternion():
     angles = np.deg2rad(np.arange(360))
     quats = z2quaternion(angles)
